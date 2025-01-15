ef04686bbfceadeb7ab907118cd8f7d07013f5a6
Blueberry012 <156487524+Blueberry012@users.noreply.github.com>
2025-01-15 22:28:49 +0100

    Update 9_ARIMA.py

--- pages/9_ARIMA.py
+++ pages/9_ARIMA.py
@@ -1,184 +1,266 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.stattools import kpss
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.stattools import adfuller
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-import numpy as np
 
 st.title("Auto-Regressive Integrated Moving Average")
 
-#Importer les données
 #df=pd.read_csv('D:\A3\DataScience\data\TP6_dataset.csv')
 df=pd.read_csv("data\cleaned_data.csv")
 new_columns = ['Pays','Année','Abonnements téléphone (cartes prépayés)','Investissements télécommunications (USD)','Abonnements téléphone (100 habitants)',"Lignes d'accès téléphoniques",'Recettes télécommunication (USD)',"Voies d'accès de communication (100 habitants)"]
 df.columns = new_columns
 df = df.drop_duplicates()
 
-#df['Pays'].unique()
+st.header("01 - Préparation des données")
+st.subheader("Paramètres")
+unique_countries = df['Pays'].unique().tolist()
+selected_country = st.selectbox('Choisir un pays :', unique_countries)
 
-#Mode
-mode = ["Exploration","Guide"]
-selected_mode = st.sidebar.selectbox('Choisir un mode :', mode)
+column_names = df.columns.tolist()
+column_names.remove("Pays")
+column_names.remove("Année")
+selected_column = st.selectbox('Choisir une variable :', column_names)
+column_names.remove(selected_column)
 
-if selected_mode =="Exploration": 
-    #Exploration Arima
-    st.subheader("Paramètres")
-    unique_countries = df['Pays'].unique().tolist()
-    selected_country = st.selectbox('Choisir un pays :', unique_countries)
+df1=df.copy()
+keepcol=['Pays','Année',selected_column]
+df1 = df1[keepcol]
 
-    column_names = df.columns.tolist()
-    column_names.remove("Pays")
-    column_names.remove("Année")
-    selected_column = st.selectbox('Choisir une variable :', column_names)
-    column_names.remove(selected_column)
-
-    df1=df.copy()
-    keepcol=['Pays','Année',selected_column]
-    df1 = df1[keepcol]
-
-    #
-    df2=df1.copy()
-    df2 = df2.drop_duplicates()
-    df2 = df2.dropna()
-    st.subheader("Dataset")
-    st.write(df2)
+df2=df1.copy()
+df2 = df2.drop_duplicates()
+df2 = df2.dropna()
     
-    df3=df2.copy()
-    df3 = df3[df3["Pays"] == selected_country]
-    del_columns=["Pays"]
-    df3 = df3.drop(columns=del_columns)
+df3=df2.copy()
+df3 = df3[df3["Pays"] == selected_country]
+del_columns=["Pays"]
+df3 = df3.drop(columns=del_columns)
 
-    df4=df3.copy()
-    df4['Année'] = pd.to_datetime(df4['Année'], format='%Y')
-    df4.set_index('Année', inplace=True)
+df4=df3.copy()
+df4['Année'] = pd.to_datetime(df4['Année'], format='%Y')
+df4.set_index('Année', inplace=True)
 
-    st.subheader("Visualisation")
+st.subheader("Dataset")
+st.write(df4)
+
+mode = ["Prévision","Précision"]
+mode_selected = st.sidebar.selectbox('Choisir le mode :', mode)
+
+if mode_selected =="Prévision":
+    st.divider()
+
+    st.header("02 - Visualisation")
     plt.figure()
     df4.plot()
-    plt.title(selected_column)
+    plt.title('Evolution')
     plt.xlabel('Year')
-    plt.ylabel(selected_column)
+    st.pyplot(plt)
+    st.divider()
+
+    st.header("03 - Test de stationnarité : Augmented Dickey-Fuller (ADF)")
+    result = adfuller(df4[selected_column])
+    st.write("P-value :", result[1])
+    if result[1] <= 0.05:
+        st.write("reject the null hypothesis. Data is stationary")
+    else:
+        st.write("weak evidence against null hypothesis,Data is non-stationary ")
+
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6))
+    plot_acf(df4[selected_column], lags=10, ax=ax[0])
+    ax[0].set_title("Autocorrelation Function (ACF)")
+
+    plot_pacf(df4[selected_column], lags=10, ax=ax[1], method="ywm")
+    ax[1].set_title("Partial Autocorrelation Function (PACF)")
+    plt.tight_layout()
+    st.pyplot(fig)
+    st.divider()
+
+    st.header("04 - Transformation: Logarithme et Différentiation")
+    st.subheader("Visualisation")
+    df_trans = df4[selected_column]
+    diff = df_trans.diff().dropna()
+    df_diff = pd.DataFrame(diff)
+
+    plt.figure()
+    df_diff.plot()
+    plt.title('Evolution avec différenciation')
+    plt.xlabel('Year')
     st.pyplot(plt)
 
-    st.subheader("Test de Stationnarité")
-    result = kpss(df4[selected_column])
-    st.write('KPSS Statistic:', result[0])
-    st.write('p-value:', result[1])
+    st.subheader("Test de stationnarité")
+    result = adfuller(df_diff[selected_column])
+    st.write("P-value :", result[1])
+    if result[1] <= 0.05:
+        st.write("reject the null hypothesis. Data is stationary")
+    else:
+        st.write("weak evidence against null hypothesis,Data is non-stationary ")
 
-    #model = ARIMA(df4[selected_column], order=(1, 1, 1))
-    #model_fit = model.fit()
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6))
+    plot_acf(df_diff[selected_column], lags=10, ax=ax[0])
+    ax[0].set_title("Autocorrelation Function (ACF)")
 
+    plot_pacf(df_diff[selected_column], lags=10, ax=ax[1], method="ywm")
+    ax[1].set_title("Partial Autocorrelation Function (PACF)")
+    plt.tight_layout()
+    st.pyplot(fig)
+    st.divider()
+
+    st.header("05 - Prédiction du modèle")
+    # Ajustement du modèle ARIMA
+    p,d,q=1,1,1
+    model = ARIMA(df_diff, order=(p, d, q))
+    model_fit = model.fit()
+    #Résumé du modèle
     #st.write(model_fit.summary())
 
-    df=df3.copy()
-    df['Année'] = pd.to_datetime(df['Année'], format='%Y')
-    df.set_index('Année', inplace=True)
+    #Prévisions (par exemple, 5 périodes dans le futur)
+    forecast = model_fit.forecast(steps=5)
+    #Revenir à l'échelle initiale
+    # Inverser la différenciation
+    last_value = df_trans.iloc[-1] # Dernière valeur log-transformée avant prédiction
+    forecast_original_scale = forecast.cumsum() + last_value
+    #forecast_original_scale = np.exp(forecast)
 
-    # Séparer les données en train (1996-2016) et test (2017-2018)
-    train = df[df.index < '2013-01-01']
-    test = df[df.index >= '2013-01-01']
+    col1,col2 = st.columns(2)
+    with col1:
+        st.subheader("Prévisions")
+        st.write(forecast)
+    with col2:
+        st.subheader("Prévisions en échelle originale")
+        st.write(forecast_original_scale)
 
-    # Entraîner le modèle ARIMA sur l'ensemble d'entraînement
-    model = ARIMA(train[selected_column], order=(1, 1, 1))
-    model_fit = model.fit()
+    # créer les dates suivantes
+    date=pd.date_range(start=df4.index[-1], periods=6, freq='YE')[1:]
+    #créer le dataframe de prédictions
+    forecast_df = pd.DataFrame({
+    "Date": date,
+    "Prévisions": forecast_original_scale
+    })
+    forecast_df.set_index('Date', inplace=True)
 
-    # Prédire pour les années 2017 et 2018
-    forecast = model_fit.forecast(steps=len(test))
-    forecast_index = test.index
-
-    # Évaluer la précision
-    st.subheader("Précisiuon du modèle")
-    mae = mean_absolute_error(test[selected_column], forecast)
-    rmse = np.sqrt(mean_squared_error(test[selected_column], forecast))
-    st.write(f"Mean Absolute Error (MAE): {mae:.2f}")
-    st.write(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
-
-    # Plot des résultats
-    st.subheader("Plot des résultats")
     plt.figure(figsize=(10, 6))
-    plt.plot(train.index, train[selected_column], label='Entraînement', color='blue', marker='o')
-    plt.plot(test.index, test[selected_column], label='Vraies valeurs (test)', color='green', marker='o')
-    plt.plot(forecast_index, forecast, label='Prédictions (test)', color='red', linestyle='--', marker='x')
-
-    plt.title('Prédictions ARIMA vs Vraies Valeurs', fontsize=14)
-    plt.xlabel('Année', fontsize=12)
-    plt.ylabel(selected_column, fontsize=12)
+    plt.plot(df4.index, df4[selected_column], label=f"{selected_column} réelles")
+    plt.plot(forecast_df.index, forecast_df['Prévisions'],
+    label="Prévisions", linestyle='--')
     plt.legend()
-    plt.grid(True)
+    plt.title("Prévisions ARIMA")
+    plt.xlabel("Date")
+    plt.ylabel("Ventes")
+    plt.xticks(rotation=45)
     st.pyplot(plt)
 
-elif selected_mode =="Guide":
-    df1=df.copy()
-    keepcol=['Pays','Année',"Abonnements téléphone (100 habitants)"]
-    df1 = df1[keepcol]
+elif mode_selected =="Précision":
+    train = df4[df4.index < '2018-01-01']
+    test = df4[df4.index >= '2018-01-01']
 
-    #
-    df2=df1.copy()
-    df2 = df2.drop_duplicates()
-    df2 = df2.dropna()
-    st.subheader("Dataset")
-    st.write(df2)
+    col1,col2 = st.columns(2)
+    with col1:
+        st.subheader("Train")
+        st.write(train)
+    with col2:
+        st.subheader("Test")
+        st.write(test)
+    st.divider()
 
-    #
-    df3=df2.copy()
-    df3 = df3[df3["Pays"] == "France"]
-    del_columns=["Pays"]
-    df3 = df3.drop(columns=del_columns)
-
-    #
-    df4=df3.copy()
-    df4['Année'] = pd.to_datetime(df4['Année'], format='%Y')
-    df4.set_index('Année', inplace=True)
-
-    #
-    st.subheader("Visualisation")
+    st.header("02 - Visualisation")
+    plt.figure()
     df4.plot()
-    plt.title('Abonnements Téléphone (100 habitants)')
+    plt.title('Evolution')
     plt.xlabel('Year')
-    plt.ylabel('Abonnements téléphone (100 habitants)')
-    plt.show()
+    st.pyplot(plt)
+    st.divider()
 
-    #
-    st.subheader("Test de Stationnarité")
-    result = kpss(df4['Abonnements téléphone (100 habitants)'])
-    st.write('KPSS Statistic:', result[0])
-    st.write('p-value:', result[1])
+    st.header("03 - Test de stationnarité : Augmented Dickey-Fuller (ADF)")
+    result = adfuller(train[selected_column])
+    st.write("P-value :", result[1])
+    if result[1] <= 0.05:
+        st.write("reject the null hypothesis. Data is stationary")
+    else:
+        st.write("weak evidence against null hypothesis,Data is non-stationary ")
 
-    df=df3.copy()
-    df['Année'] = pd.to_datetime(df['Année'], format='%Y')
-    df.set_index('Année', inplace=True)
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6))
+    plot_acf(train[selected_column], lags=10, ax=ax[0])
+    ax[0].set_title("Autocorrelation Function (ACF)")
 
-    # Séparer les données en train (1996-2016) et test (2017-2018)
-    train = df[df.index < '2013-01-01']
-    test = df[df.index >= '2013-01-01']
+    plot_pacf(train[selected_column], lags=10, ax=ax[1], method="ywm")
+    ax[1].set_title("Partial Autocorrelation Function (PACF)")
+    plt.tight_layout()
+    st.pyplot(fig)
+    st.divider()
 
-    # Entraîner le modèle ARIMA sur l'ensemble d'entraînement
-    model = ARIMA(train['Abonnements téléphone (100 habitants)'], order=(1, 1, 1))
+    st.header("04 - Transformation: Logarithme et Différentiation")
+    st.subheader("Visualisation")
+    df_trans = train[selected_column]
+    diff = df_trans.diff().dropna()
+    df_diff = pd.DataFrame(diff)
+
+    plt.figure()
+    df_diff.plot()
+    plt.title('Evolution avec différenciation')
+    plt.xlabel('Year')
+    st.pyplot(plt)
+
+    st.subheader("Test de stationnarité")
+    result = adfuller(df_diff[selected_column])
+    st.write("P-value :", result[1])
+    if result[1] <= 0.05:
+        st.write("reject the null hypothesis. Data is stationary")
+    else:
+        st.write("weak evidence against null hypothesis,Data is non-stationary ")
+
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6))
+    plot_acf(df_diff[selected_column], lags=10, ax=ax[0])
+    ax[0].set_title("Autocorrelation Function (ACF)")
+
+    plot_pacf(df_diff[selected_column], lags=10, ax=ax[1], method="ywm")
+    ax[1].set_title("Partial Autocorrelation Function (PACF)")
+    plt.tight_layout()
+    st.pyplot(fig)
+    st.divider()
+
+    st.header("05 - Prédiction du modèle")
+    # Ajustement du modèle ARIMA
+    p,d,q=1,1,1
+    model = ARIMA(df_diff['Abonnements téléphone (100 habitants)'], order=(p, d, q))
     model_fit = model.fit()
-
-    # Prédire pour les années 2017 et 2018
+    #Résumé du modèle
+    #print(model_fit.summary())
     forecast = model_fit.forecast(steps=len(test))
     forecast_index = test.index
 
-    # Évaluer la précision
-    st.subheader("Précisiuon du modèle")
-    mae = mean_absolute_error(test['Abonnements téléphone (100 habitants)'], forecast)
-    rmse = np.sqrt(mean_squared_error(test['Abonnements téléphone (100 habitants)'], forecast))
-    st.write(f"Mean Absolute Error (MAE): {mae:.2f}")
-    st.write(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
+    #Revenir à l'échelle initiale
+    # Inverser la différenciation
+    last_value = df_trans.iloc[-1] # Dernière valeur log-transformée avant prédiction
+    forecast_original_scale = forecast.cumsum() + last_value
+    #forecast_original_scale = np.exp(forecast)
 
-    # Plot des résultats
-    st.subheader("Plot des résultats")
+
+    st.subheader("Prévisions en échelle originale")
+    st.write(forecast_original_scale)
+    st.subheader("Valeurs Réelles")
+    st.write(test)
+
+    # créer les dates suivantes
+    date=test.index
+    #créer le dataframe de prédictions
+    forecast_df = pd.DataFrame({
+    "Date": date,
+    "Prévisions": forecast_original_scale
+    })
+    forecast_df.set_index('Date', inplace=True)
+
     plt.figure(figsize=(10, 6))
-    plt.plot(train.index, train['Abonnements téléphone (100 habitants)'], label='Entraînement', color='blue', marker='o')
-    plt.plot(test.index, test['Abonnements téléphone (100 habitants)'], label='Vraies valeurs (test)', color='green', marker='o')
-    plt.plot(forecast_index, forecast, label='Prédictions (test)', color='red', linestyle='--', marker='x')
-
-    # Titres et légendes
-    plt.title('Prédictions ARIMA vs Vraies Valeurs', fontsize=14)
-    plt.xlabel('Année', fontsize=12)
-    plt.ylabel('Abonnements téléphone (100 habitants)', fontsize=12)
+    plt.plot(df4.index, df4['Abonnements téléphone (100 habitants)'], label="Abonnements téléphone (100 habitants) réelles", marker='o')
+    plt.plot(forecast_df.index, forecast_df['Prévisions'], marker='o',label="Prévisions", linestyle='--', color='red')
     plt.legend()
-    plt.grid(True)
+    plt.title("Prévisions ARIMA")
+    plt.xlabel("Date")
+    plt.ylabel("Ventes")
+    plt.xticks(rotation=45)
     st.pyplot(plt)
+
+    # Évaluer la précision
+    mae = mean_absolute_error(test['Abonnements téléphone (100 habitants)'], forecast_original_scale)
+    st.write(f"Mean Absolute Error (MAE): {mae:.2f}")
